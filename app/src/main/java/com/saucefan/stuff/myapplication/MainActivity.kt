5101b250ceb77dddad02b77f33ae5d588c429621
jack seymour <stilljack@gmail.com>
2019-10-11 10:51:51 -0700

    code clean up while i rate for my stupid screen broke samsung galaxy to charge
    why you say? cause my day to day LG doesn't like android studio/adb bridge and I'm already frustrated enough

--- app/src/main/java/com/saucefan/stuff/myapplication/MainActivity.kt
+++ app/src/main/java/com/saucefan/stuff/myapplication/MainActivity.kt
@@ -6,7 +6,6 @@ import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
-import androidx.appcompat.app.AppCompatDelegate.FEATURE_SUPPORT_ACTION_BAR
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -15,13 +14,10 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import android.view.Menu
 import android.widget.Toast
-import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
-import com.google.android.gms.maps.model.CircleOptions
-import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
@@ -37,15 +33,67 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         setContentView(R.layout.activity_main)
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
+        //make map ready
         mapFragment.getMapAsync(this)
+            //check permissions
         chkperms()
+        //actionbar
         setSupportActionBar(findViewById(R.id.toolbar))
-
-         mediaPlayer = MediaPlayer.create(this, R.raw.mud)
+        //stupid mud sound effect
+        mediaPlayer = MediaPlayer.create(this, R.raw.mud)
 
     }
 
-   private fun chkperms() {
+
+
+
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        mMap.isMyLocationEnabled = true
+
+        mMap.setOnMapLongClickListener { latLng ->
+            mMap.addMarker(MarkerOptions().position(latLng).title("whatever"))
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.mainmenu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.center -> {
+           mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(LatLng(myLocation.latitude,myLocation.longitude),14f))
+            getLocation()
+            mediaPlayer.start()
+         /*   fusedLocationClient.lastLocation
+                .addOnSuccessListener {
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(LatLng(it.latitude,it.longitude),14f))
+                }*/
+         /*   if (myLocation != null) {
+
+            }*/
+            true
+        }
+        R.id.pin -> {
+            val latlng = mMap.cameraPosition.target
+            mMap.addMarker(MarkerOptions().position(latlng))
+        /*    fusedLocationClient.lastLocation
+                .addOnSuccessListener {
+                    mMap.addMarker(MarkerOptions().position(LatLng(it.latitude,it.longitude)))
+                }*/
+            mediaPlayer.start()
+            true
+        }
+        else -> {
+            super.onOptionsItemSelected(item)
+        }
+    }
+
+
+    private fun chkperms() {
         //check permission
         if (ContextCompat.checkSelfPermission
                 (
@@ -63,63 +111,8 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
             getLocation()
         }
 
-}
-
-
-
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
-        mMap.isMyLocationEnabled = true
-
-//            myLocation = mMap.myLocation
-
-
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.setOnMapLongClickListener { latLng ->
-            mMap.addMarker(MarkerOptions().position(latLng).title("whatever"))
-        }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.mainmenu,menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
-        R.id.center -> {
-
-
-           mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(LatLng(myLocation.latitude,myLocation.longitude),14f))
-            getLocation()
-            mediaPlayer.start()
-         /*   fusedLocationClient.lastLocation
-                .addOnSuccessListener {
-                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(LatLng(it.latitude,it.longitude),14f))
-                }*/
-         /*   if (myLocation != null) {
-
-            }*/
-            true
-        }
-
-        R.id.pin -> {
-            val latlng = mMap.cameraPosition.target
-            mMap.addMarker(MarkerOptions().position(latlng))
-        /*    fusedLocationClient.lastLocation
-                .addOnSuccessListener {
-                    mMap.addMarker(MarkerOptions().position(LatLng(it.latitude,it.longitude)))
-                }*/
-            mediaPlayer.start()
-            true
-        }
-
-        else -> {
-            super.onOptionsItemSelected(item)
-        }
-    }
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
@@ -133,7 +126,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
                 getLocation()
                 Toast.makeText(this,"${mMap.isMyLocationEnabled} + ${mMap.myLocation}",Toast.LENGTH_SHORT).show()
             }else {
-                permissiionDeniend()} // and we land here if not permissions
+                permissionsDenied()} // and we land here if not permissions
 
         }
     }
@@ -141,9 +134,8 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         if (ActivityCompat.checkSelfPermission(this,
                 android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
         ){
-            permissiionDeniend()
-            Toast.makeText(this,"denied",Toast.LENGTH_SHORT).show()
-                }
+            permissionsDenied()
+        }
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
         fusedLocationClient.flushLocations()
          fusedLocationClient.lastLocation
@@ -154,7 +146,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         }
 
     }
-    fun permissiionDeniend() {
-        //todo 1
+   private fun permissionsDenied(){
+        Toast.makeText(this,"denied",Toast.LENGTH_SHORT).show()
     }
 }
